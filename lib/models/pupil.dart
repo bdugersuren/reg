de04b6abc92624109cd93f09704f43c430012971
bdugersuren <dugersuren@gmail.com>
2021-04-23 11:15:19 +0800

    Pupil model uusgev

---
+++ lib/models/pupil.dart
@@ -0,0 +1,7 @@
+class Pupil {
+  final String classId;
+  final String firstName;
+  final String lastName;
+  final String register;
+  Pupil({this.firstName, this.lastName, this.classId, this.register});
+}
